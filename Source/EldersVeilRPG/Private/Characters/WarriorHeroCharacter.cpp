63ead0c61a93ae8176e2fdcf20729221bbee8ce3
Jadis0x <49281043+jadis0x@users.noreply.github.com>
2024-11-08 18:38:16 +0300

    set up character camera

--- Source/EldersVeilRPG/Private/Characters/WarriorHeroCharacter.cpp
+++ Source/EldersVeilRPG/Private/Characters/WarriorHeroCharacter.cpp
@@ -4,10 +4,38 @@
 #include "Characters/WarriorHeroCharacter.h"
 
 #include "MainCharacterDebugHelper.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Camera/CameraComponent.h"
+
+AWarriorHeroCharacter::AWarriorHeroCharacter()
+{
+	GetCapsuleComponent()->InitCapsuleSize(40.f, 95.f);
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(GetRootComponent());
+	CameraBoom->TargetArmLength = 180.f;
+	CameraBoom->SocketOffset = FVector(0.f, 50.f, 80.f);
+	CameraBoom->bUsePawnControlRotation = true;
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 450.f, 0.f);
+	GetCharacterMovement()->MaxWalkSpeed = 400.f;
+	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
+
+}
 
 void AWarriorHeroCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 
-	Debug::Print(TEXT("Working"));
 }
--- Source/EldersVeilRPG/Public/Characters/WarriorHeroCharacter.h
+++ Source/EldersVeilRPG/Public/Characters/WarriorHeroCharacter.h
@@ -6,11 +6,24 @@
 #include "Characters/WarriorBaseCharacter.h"
 #include "WarriorHeroCharacter.generated.h"
 
+class USpringArmComponent;
+class UCameraComponent;
 
 UCLASS()
 class ELDERSVEILRPG_API AWarriorHeroCharacter : public AWarriorBaseCharacter
 {
 	GENERATED_BODY()
+public:
+	AWarriorHeroCharacter();
 protected:
 	virtual void BeginPlay() override;
+
+private:
+#pragma region Component
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* FollowCamera;
+#pragma endregion
 };
